239ad3da73535650d2ec88279579c0ddb44ba09d
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-11-05 22:40:46 +0500

    FOUNDATIONS: Add Product Async Logic

---
+++ Bozor.Web/Services/Foundations/Products/IProductService.cs
@@ -0,0 +1,13 @@
+﻿//--------------------------------------------------
+// Copyright (c) Coalition Of Good-Hearted Engineers
+// Free To Use To Find Comfort And Peace
+//--------------------------------------------------
+using Bozor.Web.Models.Foundations.Product;
+
+namespace Bozor.Web.Services.Foundations.Products
+{
+    public interface IProductService
+    {
+        ValueTask<Product> AddProductAsync(Product product);    
+    }
+}
---
+++ Bozor.Web/Services/Foundations/Products/ProductService.cs
@@ -0,0 +1,20 @@
+﻿//--------------------------------------------------
+// Copyright (c) Coalition Of Good-Hearted Engineers
+// Free To Use To Find Comfort And Peace
+//--------------------------------------------------
+using Bozor.Web.Brokers.Storages;
+using Bozor.Web.Models.Foundations.Product;
+
+namespace Bozor.Web.Services.Foundations.Products
+{
+    public class ProductService : IProductService
+    {
+        private readonly IStorageBroker storageBroker;
+
+        public ProductService(IStorageBroker storageBroker) =>
+           this.storageBroker = storageBroker;
+
+        public async ValueTask<Product> AddProductAsync(Product product) =>
+            await this.storageBroker.InsertProductAsync(product);
+    }
+}
