a35e73fd93df1b7d254d115a005712b3d0b62b6a
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2025-06-14 13:26:21 +0500

    Part One

---
+++ Infrastructure/Services/Employes/EmployeService.cs
@@ -0,0 +1,62 @@
+﻿using Domain.Models.Employees;
+
+namespace Infrastructure.Services.Employes
+{
+    public class EmployeService
+    {
+        private Employee[] employees;
+        private int count;
+
+        public EmployeService()
+        {
+            count = 0;
+            employees = new Employee[4];
+        }
+
+        public void AddEmployee(Employee employee)
+        {
+            employees[count++] = employee;
+        }
+
+        public Employee[] GetEmployees()
+        {
+            return employees;
+        }
+
+        public Employee GetEmployeeById(int id)
+        {
+            foreach (var employee in employees)
+            {
+                if (employee != null && employee.Id == id)
+                {
+                    return employee;
+                }
+            }
+            return null;
+        }
+
+        public void UpdateEmployee(int id, Employee updatedEmployee)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (employees[i].Id == id)
+                {
+                    employees[i] = updatedEmployee;
+                    return;
+                }
+            }
+        }
+
+        public void DeleteEmployee(int id)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (employees[i].Id == id)
+                {
+                    employees[i] = null;
+                    return;
+                }
+            }
+        }
+    }
+}
