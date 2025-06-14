ff0673625ad5194cde425008438181c88836d569
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2025-06-14 12:44:27 +0500

    CODE RUB: Delete Infrastructure Project

---
+++ Domain/Models/Employees/Employee.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Models.Departments;
+using System;
+
+namespace Domain.Models.Employees
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public int Age { get; set; }
+
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Id: {Id}, Name: {FullName}, Age: {Age}, Department: {Department?.Name}");
+        }
+    }
+}
